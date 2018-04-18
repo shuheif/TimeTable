13736d98def3992499509fe0bc8fa030437b67cc
= <fujitas@beloit.edu>
2018-04-18 11:54:49 -0500

    Formating AppDelegate file.

--- TimeTable/AppDelegate.swift
+++ TimeTable/AppDelegate.swift
@@ -16,45 +16,33 @@ import Ensembles
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
 
     var window: UIWindow?
-    
     let eventStore = EventStore.shared
     let userDefaults = UserDefaults.standard
     let defaultsDict: [String: AnyObject] = ["themaColor": 0 as AnyObject, "purchased": false as AnyObject]
     let defaultStack = CoreDataStack.shared
     let gregorianCalendar: Calendar = Calendar(identifier: Calendar.Identifier.gregorian)
-    
     var selectedUUID: String? = nil
     let days: [String] = [NSLocalizedString("MF", comment: "月-金"), NSLocalizedString("MSat", comment: "月-土"), NSLocalizedString("MSun", comment: "月-日")]//これはそのままエンティティに格納されるので、アップデートでも変更不可。
     let numOfClasses: [Int] = [4, 5, 6, 7, 8, 9, 10, 11, 12]
     let detailColors = ["Purple", "Pink", "Rose", "Capucine", "Orange", "Yellow", "LightGreen", "EmeraldGreen", "CadetBlue", "LightBlue", "Blue", "Navy", "Black"]
     let themeColors: [String] = ["Jade", "PeterRiver", "SkyBlue", "Amethyst", "Sakura", "Alizarin",  "Orange", "SunFlower", "Asbestos"]
-    
     let dayName = [NSLocalizedString("Mon", comment: "月曜"), NSLocalizedString("Tue", comment: "火曜"), NSLocalizedString("Wed", comment: "水曜"), NSLocalizedString("Thu", comment: "木曜"), NSLocalizedString("Fri", comment: "金曜"), NSLocalizedString("Sat", comment: "土曜"), NSLocalizedString("Sun", comment: "日曜")]
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+        // Use verbose logging for sync
         CDESetCurrentLoggingLevel(CDELoggingLevel.verbose.rawValue)
-        
         // Override point for customization after application launch
         userDefaults.register(defaults: defaultsDict)
-        
-        // Setup Ensemble
         defaultStack.enableEnsembles()
         defaultStack.sync(completion: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.localSaveOccured(notification:)), name: .CDEMonitoredManagedObjectContextDidSave, object: nil)
-        defaultStack.sync(completion: nil)
-        
-        //EventKit
+        defaultStack.sync(completion: nil)        //EventKit
         eventStore.checkAuthorizationStatus()
-        
         SKPaymentQueue.default().add(StoreKitAccessor.shared)
-        
         //Firebase
         FirebaseApp.configure()
-
         print("premium: \(alreadyPurchased)")
-        
         let splitViewController = self.window!.rootViewController as! UISplitViewController
         let detailNavigationController = splitViewController.viewControllers.last as! UINavigationController
         let detailViewController = detailNavigationController.topViewController as! TimeTableViewController
@@ -65,19 +53,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         } else {
             splitViewController.preferredDisplayMode = .automatic
         }
-        
         splitViewController.delegate = self
-        
         return true
     }
     
-
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
@@ -88,7 +72,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         }
         SKPaymentQueue.default().remove(StoreKitAccessor.shared)
     }
-
     
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
@@ -96,12 +79,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         SKPaymentQueue.default().add(StoreKitAccessor.shared)
     }
 
-    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         defaultStack.sync(completion: nil)
     }
-
     
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
@@ -109,15 +90,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         defaultStack.saveContext()
     }
 
-    
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
         NSLog("Received a remote notification")
     }
     
     // MARK: - Split view
-
     func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
-        
         let secondaryAsNavController = secondaryViewController as! UINavigationController
         let detailViewController = secondaryAsNavController.topViewController as! TimeTableViewController
         if detailViewController.timetable == nil {
@@ -127,15 +105,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         }
     }
  
-    
     @objc func localSaveOccured(notification: Notification) {
         print("AppDelegate: Local save occured")
         defaultStack.sync(completion: nil)
     }
  
-    
     // MARK: - In App Purchase
-    
     var alreadyPurchased: Bool = {
         let icloudKeyValueStore = NSUbiquitousKeyValueStore.default
         let icloud = icloudKeyValueStore.bool(forKey: "purchased")
@@ -150,6 +125,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
             return false
         }
     }()
-    
 }
 
